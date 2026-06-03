11872fad22d9db530debb82f6fd7dcb6ab8e7d8d
Anrietta <sekereshai@gmai.com>
2026-06-03 23:54:37 +0300

    Add feature -> focus autoshift on group of phone number inputs Add form reset after sumbit

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,33 @@
 const form = document.querySelector('#form');
 const formElsObj = {};
 
+// хочу зробити так, щоб при введенні номеру телефону фокус сам переходив на наступний інпут, бо ручний таб дратує.
+//план:
+//1. отримаю всі інпути де атр name починається з phone-number
+//2. на кожен повішаю подію oninput чи onchange 
+//3. коли length === maxlength інпута перенесу фокус на наступний інпут
+
+// oninput - реагує миттєво на кожну зміну в інпуті
+// onchange - реагує один раз коли редагування завершено (коли елемент втратив фокус)
+
+const phoneNumberEls = Array.from(form.querySelectorAll('[name^="phone-number"]'));
+
+function autoFocusHandler(e) {
+    const targetIndex = phoneNumberEls.indexOf(e.target);
+    // debugger;
+
+    // maxLength - не забувти про перехід на camelCase коли отримую властивості елемента (атрибут maxlength === властивість maxLength)
+    if (e.target.value.length === e.target.maxLength) {
+        const nextTarget = phoneNumberEls[targetIndex + 1];
+
+        if (nextTarget) nextTarget.focus();
+    }
+}
+
+phoneNumberEls.forEach(el => el.addEventListener('input', autoFocusHandler))
+
+
+
 function submitHandler(e) {
     e.preventDefault();
 
@@ -22,7 +49,9 @@ function submitHandler(e) {
         }
     })
 
+    //скидаю всі поля форми після submit до початкових (reset - це метод HTMLFormElement)
+    this.reset();
 }
 
 form.addEventListener('submit', submitHandler);
-console.log(formElsObj);
+
